e477dd5057a36fe03668082c986822bb011a7f05
martemoslet <martemoslet@gmail.com>
2023-05-15 15:21:30 +0200

    Adds nav content

---
+++ src/components/ui/Nav.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/contactPage">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
