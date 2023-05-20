6ba37fb399987ed18d37ebc89549be4c23ee459a
martemoslet <martemoslet@gmail.com>
2023-05-20 14:54:25 +0200

    Adds routes and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { Routes, Route, Outlet } from "react-router-dom";
-import Header from "./components/ui/Header";
-import Footer from "./components/ui/Footer";
-import Nav from "./components/ui/Nav";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
 import Contact from "./pages/ContactPage";
 import Home from "./pages/Home";
+import SingleProduct from "./pages/ProductPage";
 
-function Layout() {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
-  );
-}
 
 export default function App() {
   return (
     <div>
-      <Header>
-        <Nav />
+
         <Routes>
-          
+          <Route path="/" element={<Layout/>}>
             <Route index element={<Home />} />
             <Route path="contactPage" element={<Contact />} />
- 
+            <Route path="ProductPage/:id" element={<SingleProduct />} />
+          </Route>
         </Routes>
-      </Header>
-      <Footer bg="LavenderBlush" text="DarkOrchid" padding="50px">
-        Copyright {new Date().getFullYear()}
-      </Footer>
+
     </div>
   );
 }
