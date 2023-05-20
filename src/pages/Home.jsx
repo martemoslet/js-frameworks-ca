6606d745b1744e24c5a2f5843fe5e97d1edd86b4
martemoslet <martemoslet@gmail.com>
2023-05-20 14:55:41 +0200

    Adds local storage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 // import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import ProductList from '../components/ProductList';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+import "./Home.css"
 
 const url = 'https://api.noroff.dev/api/v1/online-shop';
 
 export default function Home() {
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useLocalStorage("products", []);
 
     useEffect(() => {
         async function getData() {
