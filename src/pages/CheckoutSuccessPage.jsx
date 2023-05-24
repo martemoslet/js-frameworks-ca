04d2609526dc8d894e8d644f06b545ea1a88e838
martemoslet <martemoslet@gmail.com>
2023-05-24 21:06:53 +0200

    Adds basic code

---
+++ src/pages/CheckoutSuccessPage.jsx
@@ -0,0 +1,13 @@
+import styles from "../components/Button.module.css"
+import { Link } from 'react-router-dom';
+
+export default function CheckoutSuccessPage() {
+    return (
+        <div>
+        <h1>Checkout success</h1>
+        <Link to="/">
+            <button className={styles.primary}>Back to home page</button>
+        </Link>
+    </div>
+    );
+}
