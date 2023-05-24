6599d5afaaf9ef9465d445c2f8efded59ccec89e
martemoslet <martemoslet@gmail.com>
2023-05-24 21:06:13 +0200

    Updates styles

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,14 +1,13 @@
 import { styled } from "styled-components"
 
 const StyledHeader = styled.header`
-background-color: lavender;
-color: deeppink;
+background-color: rgb(244, 244, 244);
+color: white;
 padding: 20px;
 `
 
 export default function Header(props) {
     return (<StyledHeader>
-        <h1>Header</h1>
         {props.children}
         </StyledHeader>)
 }
