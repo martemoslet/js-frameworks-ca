d6c6993f09b2031fb3e26b60de2d13a672b28fb6
martemoslet <martemoslet@gmail.com>
2023-05-29 13:03:48 +0200

    Adds code for reviews

---
+++ src/components/Review.jsx
@@ -0,0 +1,9 @@
+export default function Review({ username, description }) {
+
+    return (
+        <div>
+            <h3>User{username}</h3>
+            <p>Description{description}</p>
+        </div>
+    );
+  }
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,5 @@
+import Review from "./Review"
+
+export default function Reviews({ reviews }) {
+    return reviews && reviews.map((review) =>((<Review {...review} />)))}
+
