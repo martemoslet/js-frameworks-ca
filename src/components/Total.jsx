ecb516a9599a0e2b57a7330a980d3bcf1c91d58b
martemoslet <martemoslet@gmail.com>
2023-06-04 14:34:39 +0200

    Adds code for total price

---
+++ src/components/Total.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { CartContext } from "../components/CartProvider"
+
+export default function Total () {
+    const { total } = useContext(CartContext);
+    return (
+        <div>
+        Total: {parseFloat(total).toFixed(2)} NOK
+    </div>
+      );
+}
