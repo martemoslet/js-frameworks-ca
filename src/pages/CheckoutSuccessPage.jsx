41e864b20cb96945602ef65f03b3b4aa023e1bf8
martemoslet <martemoslet@gmail.com>
2023-06-04 14:36:40 +0200

    Adds class

--- src/pages/CheckoutSuccessPage.jsx
+++ src/pages/CheckoutSuccessPage.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 export default function CheckoutSuccessPage() {
     return (
-        <div>
-        <h1>Checkout success</h1>
+        <div className="checkout-card">
+        <h1>Checkout success!</h1>
         <Link to="/">
             <button className={styles.primary}>Back to home page</button>
         </Link>
