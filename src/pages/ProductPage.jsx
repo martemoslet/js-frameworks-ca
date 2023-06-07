f3b0d1de102ab6065ca2e73fa38e770bcab67f20
martemoslet <martemoslet@gmail.com>
2023-06-07 21:31:18 +0200

    Adds fetch to page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,7 +1,44 @@
 import SingleProduct from "../components/ProductPage";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function ProductPage() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  let { id } = useParams();
+
+  useEffect(() => {
+    async function getData(url) {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+
+        const response = await fetch(url);
+        const json = await response.json();
+
+        setData(json);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getData(`https://api.noroff.dev/api/v1/online-shop/${id}`);
+  }, [id]);
+
+  if (isLoading || !data) {
+    return <div>Loading</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  console.log(data);
+
   return (
-      <SingleProduct />
+      <SingleProduct key={data.id} {...data} />
   );
 }
