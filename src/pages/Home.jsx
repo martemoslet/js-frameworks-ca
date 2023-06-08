cd969bc3e0ae35ba96929103c9246f416cd39c85
martemoslet <martemoslet@gmail.com>
2023-06-08 13:13:31 +0200

    Removes unused data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,15 @@ import { useLocalStorage } from "../hooks/useLocalStorage";
 import SearchBar from "../components/Search";
 import SearchList from "../components/SearchList";
 
-const url = "https://api.noroff.dev/api/v1/online-shop";
+//const url = "https://api.noroff.dev/api/v1/online-shop";
 
 export default function Home() {
   const [products] = useLocalStorage("products", []);
 
   useEffect(() => {
     async function getData() {
-      const response = await fetch(url);
-      const json = await response.json();
+      //const response = await fetch(url);
+      //const json = await response.json();
       //setProducts(json);
     }
     getData();
